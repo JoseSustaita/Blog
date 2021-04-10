a6a9ccc2450b3cd31e6e201cf8a80c6711f6a77a
Jose Sustaita <josesustaita70@gmail.com>
2021-04-10 17:56:30 -0600

    Initial Set UP
    Connected Vue, added auth, make models and migrations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+import Vue from "vue";
+import App from "./Vue/app.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        App
+    }
+});
